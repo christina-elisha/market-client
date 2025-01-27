497cebebee98c724c75917ff4ac0515bed12036d
rex <101697136+christina-elisha@users.noreply.github.com>
2025-01-27 08:24:06 -0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,301 @@
+/*import 'package:flutter/material.dart';
+import 'package:flutter/src/material/search_anchor.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Discover',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        useMaterial3: true,
+      ),
+      home: const Discover(title: 'Discover'),
+    );
+  }
+}
+
+class Discover extends StatefulWidget {
+  const Discover({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<Discover> createState() => _DiscoverState();
+}
+
+class _DiscoverState extends State<Discover> {
+  bool isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.navigate_next_outlined),
+              label: 'navigate'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border),
+              label: 'favorite'),
+        ],
+        backgroundColor: Theme.of(context).primaryColorLight,
+      ),
+      body:
+        const Center(
+          //child: Icon(Icons.map_sharp),
+          child: SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  padding: MaterialStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0)),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                );
+              },
+              suggestionsBuilder:
+              (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          })
+      ),
+    );
+  }
+}*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_swipe_button/flutter_swipe_button.dart';
+
+/// Flutter code sample for [SearchBar].
+
+void main() => runApp(const SearchBarApp());
+
+class SearchBarApp extends StatefulWidget {
+  const SearchBarApp({super.key});
+
+  @override
+  State<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends State<SearchBarApp> {
+  bool isDark = false;
+  late int selectedStoreId;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        brightness: isDark ? Brightness.dark : Brightness.light);
+
+    return MaterialApp(
+      theme: themeData,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.navigate_next_outlined),
+                label: 'navigate'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border),
+                label: 'favorite'),
+          ],
+          backgroundColor: Theme.of(context).primaryColorLight,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Stack(
+            children: <Widget>[
+              const Center(
+                  // google map api to replace Icon
+                  child: Icon(Icons.directions_bike)
+              ),
+              DraggableScrollableSheet(
+                  initialChildSize: 0.4,
+                  minChildSize: 0.4,
+                  maxChildSize: 0.9,
+                  builder: (context, scrollController) {
+                    return ListView.separated(
+                        controller: scrollController,
+                        itemBuilder: (_, index) =>
+                          ElevatedButton(
+                            onPressed: () {
+                              selectedStoreId = index;
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => StoreDetailPage(
+                                    storeId: selectedStoreId
+                                  )));
+                            },
+                              child: Text('Store $index')),
+                        separatorBuilder: (_, index) =>
+                          const Divider(),
+                        itemCount: 20);
+                  }),
+              SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0)),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                  trailing: <Widget>[
+                    Tooltip(
+                      message: 'Change brightness mode',
+                      child: IconButton(
+                        isSelected: isDark,
+                        onPressed: () {
+                          setState(() {
+                            isDark = !isDark;
+                          });
+                        },
+                        icon: const Icon(Icons.wb_sunny_outlined),
+                        selectedIcon: const Icon(Icons.brightness_2_outlined),
+                      ),
+                    )
+                  ],
+                );
+              }, suggestionsBuilder:
+              (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          }),
+        ]
+        ),
+      ),
+    )
+    );
+  }
+}
+
+class StoreDetailPage extends StatefulWidget{
+  final int storeId;
+  const StoreDetailPage({super.key, required this.storeId});
+
+  @override
+  State<StoreDetailPage> createState() => _StoreDetailPageState();
+}
+
+class _StoreDetailPageState extends State<StoreDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('store ${widget.storeId}')),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('item'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 8,
+                      ),
+                      child: SwipeButton(
+                        trackPadding: const EdgeInsets.all(6),
+                        activeThumbColor: Colors.blue,
+                        activeTrackColor: Colors.blue[100],
+                        thumb: const Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
+                        ),
+                        elevationThumb: 2,
+                        elevationTrack: 2,
+                        child: const Text(
+                          "Swipe to buy...",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onSwipe: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Swiped"),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        },
+                      ),
+                    ),]
+          ),
+        ),
+
+    );
+  }
+}
